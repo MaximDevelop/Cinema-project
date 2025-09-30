0e4345d929688f95cd4c654e7f3568dfb0433995
MaximDevelop <webmaxprogram@gmail.com>
2025-09-30 21:56:29 +0500

    Адаптив

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,12 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
+const videoTrailer = document.querySelector(".video-trailer");
+const videoButton = document.querySelector(".film-trailer");
 let cost = 800;
 let totalPrice = 0;
+
 schemeSvg.addEventListener("click", (event) => {
     if(!event.target.classList.contains("booked")){
         event.target.classList.toggle("active");
@@ -9,4 +14,13 @@ schemeSvg.addEventListener("click", (event) => {
         totalPrice = totalSeats * cost;
         totalPriceTag.textContent = totalPrice;
     }
+});
+
+menuButton.addEventListener('click', () => {
+   menu.classList.toggle('is-open');
+});
+
+videoButton.addEventListener('click', () => {
+    videoTrailer.style.display = "block";
+    videoButton.style.display = "none";
 });
